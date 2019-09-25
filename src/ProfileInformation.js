e6bcc4986b7ff2885e09d18a32c07ef8e9a895a9
Aaron Anthony <aa0723@gmail.com>
2019-09-24 20:22:19 -0600

    deploying build

--- src/ProfileInformation.js
+++ src/ProfileInformation.js
@@ -2,7 +2,7 @@
 import { FaGithub, FaLinkedin, FaEnvelope, FaFileAlt } from "react-icons/fa";
 // Import image of your self here:
 //-- For best display purposes, your image should be sqaure (e.g. 300 X 300 px)
-import selfie from "./assets/trevor.png";
+import selfie from "./assets/aaron.jpg";
 
 //Import images for your portfolio here:
 //-- For best display purposes, each of these images should have similar porportions
@@ -13,20 +13,20 @@ import blogApp from "./assets/blogapp-site.png";
 import taskyv from "./assets/taskyv_site.png";
 import bookThinks from "./assets/bookThinks.png";
 
-import resume from "./assets/Resume.pdf";
+import resume from "./assets/resume.pdf";
 
 let darkModeSwitch = true;
 let darkMode = false;
 
 // Edit person info here
 // -- Bio is best kept to around 5 lines
-let firstName = "Trevor";
-let lastName = "Lane";
+let firstName = "Aaron";
+let lastName = "Anthony";
 let title = "Student / Software Developer";
-let bio = `Hey there, I'm currently studying computer science
-   at Brigham Young University and working as a Software
-   Engineering Intern at SimpleNexus.
-   Feel free to reach out, take a look at some things i've built
+let bio = `Hey there, I'm currently studying Computer Engineering
+   at Brigham Young University and working part-time as a Software
+   Engineer Intern at Qualtrics.
+   Feel free to reach out, take a look at some things I've built
    here, or checkout my code on github.`;
 
 //Edit links to external accounts or items here (e.g. github, linkedIn, leetcode, resume, etc.)
@@ -38,18 +38,18 @@ let userIcons = true;
 let links = [
   {
     name: "Email",
-    link: "mailto:trevor@trevorlane.net",
+    link: "mailto:aaron.anthony888@gmail.com",
     icon: FaEnvelope
   },
   {
     name: "GitHub",
-    link: "https://github.com/trevorhere",
+    link: "https://github.com/aaronanthony8",
     icon: FaGithub
 
   },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/hi-there-trevor-here/",
+    link: "https://www.linkedin.com/in/aaronanthony888/",
     icon: FaLinkedin
 
   },
